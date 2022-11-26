0d4dd9b9a4f7a96a831f5c0b0118deb3671684ec
GersonVis <visoso126@gmail.com>
2022-11-25 21:18:25 -0600

    avance

--- app/src/main/java/com/example/curso_android_kotlin/introduccion_android/variables_valores_y_constantes.kt
+++ app/src/main/java/com/example/curso_android_kotlin/introduccion_android/variables_valores_y_constantes.kt
@@ -5,13 +5,13 @@ import com.example.curso_android_kotlin.R
 
 
 class variables_valores_y_constantes {
-    companion object {// hace que sea accesible desde cualquier parte de la aplicación
+    companion object {//este es otro ambito
+        // hace que sea accesible desde cualquier parte de la aplicación
         // no se pueden crear dentró del proceso de creación de un elemento
         // por eso se crena dentró de otro ambito
         const val mon="EUR";// las variables constantes no son aplicables a variables locales
-
     }
-    fun onCreate(savedInstanceState: Bundle?) {
+    fun onCreate(savedInstanceState: Bundle?) {//proceso de la aplicación es un ambito
         // Kotlin e sun lenguaje compilado, se hace una reserva
         // de memoría del dato que estamos guardando
         // las variables son referencias a zonas de memoría
@@ -29,10 +29,16 @@ class variables_valores_y_constantes {
        // fecha_nacimiento="nuevo dato";//error no se pueden asignar nuevos valores a tipos de dato val
         println(nombre);
 
+/*Tipos de variables*/
+        val rfc = "BIOG8348585884"//su valor no cambia
+        var nombreArchivo = "gatito.png"//se puede cambiar su valor
+     //   rfc="AIG34343434"//Error
+        nombreArchivo = "persona.png"
 
+        //val valor
+        //var variable
+        //const val valor constante
 
-
-
-
+//kotlin es un lenguaje tipado por que la variable debe respetar su tipo de dato
     }
 }
