9ddb61e2c1ec56aa107be623498149edd5e0cb85
GersonVis <visoso126@gmail.com>
2022-12-10 23:39:05 -0600

    avance

--- programas/Objetos/app/src/main/java/com/example/objetos/MainActivity.kt
+++ programas/Objetos/app/src/main/java/com/example/objetos/MainActivity.kt
@@ -4,51 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    //agregar un nuevo metodo a la clase String
+    private fun String.nospace():String{
+        //recordar que en kotlin todos son objetos hasta los tipos de datos
+        // por eso mismo se puede acceder a los valores del string directamente
+        return this.replace(" ", "")
+    }
+    private fun Array<Int>.mostrar(){
+            println("{")
+            for(i in this) println(i)
+            println("}")
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-     /*   val subaclase:Subclases = Subclases()
-        subaclase.metodo()
+        var nombre:String="gerson visoso ocampo".nospace()
 
-        val dentroSubClase: Subclases.Dentro = Subclases().Dentro()
-        dentroSubClase.metodo()
-
-        // implementamos la data class en nuestro proyecto
-        val centauro: Star = Star("centauro", 400f, "andromeda")
-        val centauroHija: Star =centauro.copy(nombre="centauro hija")
-
-        val stars = object{
-            fun fuerzaAtraccion(distance: Float, sizeOne: Float, sizeTwo: Float): Float{
-                return (sizeOne*sizeTwo)/distance
-            }
-        }
-        var estrellasAtracion=stars.fuerzaAtraccion(4000f, centauro.radius, centauroHija.radius)
-        println(estrellasAtracion)
-        println(centauro)
-        println(centauroHija)
-
-        var hoy:Dias=Dias.MIERCOLES
-        var dias:Array<Dias> = Dias.values();
-        println(hoy.name)
-        println(hoy.ordinal)
-        println(hoy.saludo())
-
-        var rojo:Colores=Colores.ROJO
-        println(rojo)
-        println(rojo.demasValores())*/
-       /* val ciclista1: Ciclista = Ciclista(
-            "gerson",
-            1.76f,
-            90.0f,
-            27,
-            TipoBicicleta.NORMAL)*/
-           val nadador1: Nadador = Nadador(
-               "gerson",
-               1.76f,
-               90f,
-               27,
-                NadoEstilo.LIBRE)
+        var birthDays: Array<Int> = Array<Int>(10){it}
+        birthDays.mostrar()
     }
 
 }
