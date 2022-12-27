25556b713e1fd96bab2972e3dd69c943d94caf08
GersonVis <visoso126@gmail.com>
2022-12-27 17:25:04 -0600

    avance

---
+++ programas/Objetos/app/src/main/java/com/example/objetos/ActivityRelativeLayour.kt
@@ -0,0 +1,11 @@
+package com.example.objetos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityRelativeLayour : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layour)
+    }
+}
