747094c7c9a4d4b8d1d23059a63318a9303164a5
GersonVis <visoso126@gmail.com>
2022-12-27 22:02:02 -0600

    constraint layout

---
+++ programas/Objetos/app/src/main/java/com/example/objetos/ActivityConstraintLayout.kt
@@ -0,0 +1,11 @@
+package com.example.objetos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityConstraintLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
