619c25ba1291c4bee0f30c56408b018f30b6dc5e
GersonVis <visoso126@gmail.com>
2022-12-28 23:46:55 -0600

    avance

---
+++ programas/Objetos/app/src/main/java/com/example/objetos/ActivityTableLayout.kt
@@ -0,0 +1,11 @@
+package com.example.objetos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityTableLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_layout)
+    }
+}
